9b71aa4a16e3f68d8cf88d9f8d16666959d61b30
Bridget Asser <bridget.asser@gmail.com>
2019-12-02 23:25:52 -0500

    lite wireframe and psudocode

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+
+// game flow and problems to solve 
+
+//1 start the game and the pattern (init the game)
+// user clicks something on board that inits the main play sequence (function). 
+
+//the start button starts the game. 
+
+
+
+
+//2 player attempts the same pattern 
---
+++ psudocode.md
@@ -0,0 +1,13 @@
+# my work flow approaches to the game
+
+// game flow and problems to solve 
+
+//1 start the game and the pattern (init the game)
+// user clicks something on board that inits the main play sequence (function). 
+
+//the start button starts the game. 
+
+
+
+
+//2 player attempts the same pattern 
